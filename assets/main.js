65ea2c42dea324bb07c7fafd7262cff893abaacc
fdrbrs <federio.borsci@gmail.com>
2021-04-27 11:01:41 +0200

    snack2

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+//L’utente inserisce due parole in successione, con due prompt.
+
+var parola1 = prompt("scrivi una parola")
+var parola2 = prompt("scrivi un'altra parola")
+
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+if (parola1.length == parola2.length) {
+    console.log("Le parole sono delle stessa lunghezza");
+}else if (parola1.length < parola2.length) {
+    console.log(parola1 + ' ' + parola2);
+} else {
+    console.log(parola2 + ' ' + parola1);
+}
