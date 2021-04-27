05721281d251f5972d8a203cb323d05d268d4a7d
fdrbrs <federio.borsci@gmail.com>
2021-04-27 12:32:26 +0200

    snack 4

--- assets/main.js
+++ assets/main.js
@@ -17,7 +17,9 @@ var parola2 = prompt("scrivi un'altra parola");
 
 //SNACK 3
 
-var n1 = Number(prompt("1.Inserisci un numero"));
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+
+/* var n1 = Number(prompt("1.Inserisci un numero"));
 var n2 = Number(prompt("2.Inserisci un numero"));
 var n3 = Number(prompt("3.Inserisci un numero"));
 var n4 = Number(prompt("4.Inserisci un numero"));
@@ -28,9 +30,41 @@ var n8 = Number(prompt("8.Inserisci un numero"));
 var n9 = Number(prompt("9.Inserisci un numero"));
 var n10 = Number(prompt("10.Inserisci un numero"));
 
+//Il programma stampa la somma di tutti i numeri inseriti.
+
 var somma = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10;
 
-console.log(somma);
+console.log(somma); */
 
-document.getElementById("msg").innerHTML = somma;
+/* document.getElementById("msg").innerHTML = somma;
+ */
 
+//con ciclo for
+/* var somma = 0;
+for (var i = 0; i < 10; i++){
+    var numeroUtente = Number(prompt("inserisci un numero"));
+    somma += numeroUtente
+}
+
+document.getElementById("msg").innerHTML = somma; */
+
+//SNACK4
+
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
+
+var invitati = ["Henry James", "Andrew Le Vot", "Jay Gatsby", "Myrtle Wilson", "Nick Carraway", "Daisy Buchanan", "Jordan Baker", "Fabio"];
+
+//chiedi all’utente il suo nome
+
+var utente = prompt("Inserisca qua il Suo nome:");
+
+//e comunicagli se può partecipare o no alla festa.
+
+if (invitati.includes(utente)) {
+    msg = "Sei uffcialmente invitato alla festa di Gatsby";
+} else {
+    msg = "Purtroppo il Suo nome non e' in lista";
+}
+
+document.getElementById("msg").innerHTML = msg;
+console.log(msg);
