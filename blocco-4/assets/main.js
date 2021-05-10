fdbf148a53df58c3c7a44a2005291c741f2560bb
fdrbrs <federio.borsci@gmail.com>
2021-05-10 11:45:24 +0200

    snack 2b

--- blocco-4/assets/main.js
+++ blocco-4/assets/main.js
@@ -1,11 +1,20 @@
 /* 
-1a:
+Snack 1a:
 Creare un oggetto palla che abbia le seguenti proprietà.
 Nome = palla
 Peso = 10
 */
 
 var palla = {
-    nome: palla,
+    nome: 'palla',
     peso: 10
 }
+
+/* 
+Snack 1b:
+Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+*/
+
+palla.peso = Number(prompt('scrivi il peso della palla'));
+
+console.log(palla);
