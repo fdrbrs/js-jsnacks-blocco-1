35d763ffb0f10c40677a4cccb18183599c0cb5e2
fdrbrs <federio.borsci@gmail.com>
2021-05-10 12:27:49 +0200

    snack3

--- blocco-4/assets/main.js
+++ blocco-4/assets/main.js
@@ -1,3 +1,5 @@
+//SNACK JS BLOCCO 4
+
 /* 
 Snack 1a:
 Creare un oggetto palla che abbia le seguenti proprietà.
@@ -5,16 +7,40 @@ Nome = palla
 Peso = 10
 */
 
-var palla = {
+/* var palla = {
     nome: 'palla',
     peso: 10
-}
+} */
 
 /* 
 Snack 1b:
 Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
 */
 
-palla.peso = Number(prompt('scrivi il peso della palla'));
+/* palla.peso = Number(prompt('scrivi il peso della palla'));
 
-console.log(palla);
+console.log(palla); */
+
+/* 
+Snack 2:
+Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
+Calcolare perimetro e area.
+(ripassiamo il Teorema di Pitagora ;D )
+*/
+
+triangolo = {
+    base: 10,
+    altezza: 4
+}
+
+var area = (triangolo.base * triangolo.altezza) / 2;
+
+console.log(area);
+
+var catetoMaggiore = Math.sqrt(Math.pow(triangolo.base,2) - Math.pow(triangolo.altezza,2));
+
+console.log(catetoMaggiore);
+
+var perimetro = triangolo.altezza + triangolo.base + catetoMaggiore;
+
+console.log(perimetro);
